660b47e1acf549c82321530f777852af51a1ef55
Jillian Forsythe <j.forsythe8@gmail.com>
2016-05-07 15:08:18 -0700

    Try implementing API call without jQuery

--- build/main.js
+++ build/main.js
@@ -20191,17 +20191,59 @@
 	  },
 
 	  componentDidMount: function componentDidMount() {
-	    this.serverRequest = $.get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1', function (result) {
-	      console.log(result);
-	      this.setState({
-	        ghData: result.tree
+	    var _this = this;
+
+	    // this.serverRequest = $.get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1', function (result) {
+	    //   console.log(result);
+	    //   this.setState({
+	    //     ghData: result.tree,
+	    //   });
+	    // }.bind(this));
+	    function get(url) {
+	      // Return a new promise.
+	      return new Promise(function (resolve, reject) {
+	        // Do the usual XHR stuff
+	        var req = new XMLHttpRequest();
+	        req.open('GET', url);
+
+	        req.onload = function () {
+	          // This is called even on 404 etc
+	          // so check the status
+	          if (req.status == 200) {
+	            // Resolve the promise with the response text
+	            resolve(req.response);
+	          } else {
+	            // Otherwise reject with the status text
+	            // which will hopefully be a meaningful error
+	            reject(Error(req.statusText));
+	          }
+	        };
+
+	        // Handle network errors
+	        req.onerror = function () {
+	          reject(Error("Network Error"));
+	        };
+
+	        // Make the request
+	        req.send();
 	      });
-	    }.bind(this));
+	    }
+
+	    // Use it!
+	    get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1').then(JSON.parse).then(function (response) {
+	      console.log("Success!", response);
+	      return response;
+	    }, function (error) {
+	      console.error("Failed!", error);
+	    }).then(function (response) {
+	      _this.setState({ ghData: response });
+	    });
+	    console.log(this.state.ghData);
 	  },
 
-	  componentWillUnmount: function componentWillUnmount() {
-	    this.serverRequest.abort();
-	  },
+	  // componentWillUnmount: function() {
+	  //   this.serverRequest.abort();
+	  // },
 
 	  render: function render() {
 	    return _react2.default.createElement(
--- source/components/api.jsx
+++ source/components/api.jsx
@@ -3,7 +3,6 @@ import Content from './content.jsx'
 
 var Api = React.createClass({
 
-
   getInitialState: function() {
     return {
       ghData: [],
@@ -11,22 +10,66 @@ var Api = React.createClass({
   },
 
   componentDidMount: function() {
-    this.serverRequest = $.get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1', function (result) {
-      console.log(result);
-      this.setState({
-        ghData: result.tree,
+    // this.serverRequest = $.get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1', function (result) {
+    //   console.log(result);
+    //   this.setState({
+    //     ghData: result.tree,
+    //   });
+    // }.bind(this));
+    function get(url) {
+      // Return a new promise.
+      return new Promise(function(resolve, reject) {
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = function() {
+          // This is called even on 404 etc
+          // so check the status
+          if (req.status == 200) {
+            // Resolve the promise with the response text
+            resolve(req.response);
+          }
+          else {
+            // Otherwise reject with the status text
+            // which will hopefully be a meaningful error
+            reject(Error(req.statusText));
+          }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+          reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+
       });
-    }.bind(this));
+    }
+
+    // Use it!
+    get('https://api.github.com/repos/j-forsythe/red-project3-react/git/trees/master?recursive=1')
+    .then(JSON.parse)
+    .then(function(response) {
+      console.log("Success!", response);
+      return response;
+    }, function(error) {
+      console.error("Failed!", error);
+    }).then(response => {
+      this.setState({ghData: response});
+    });
+    console.log(this.state.ghData);
+
   },
 
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
+  // componentWillUnmount: function() {
+  //   this.serverRequest.abort();
+  // },
 
   render() {
     return (
       <div>
-        {/*<ul className='firstLevel'></ul>*/}
         <Content ghData={this.state.ghData} />
       </div>
     )
